6e8a9d47f2d20b159eb75229eea9f9b00eccd40d
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2026-09-13 12:33:43 -0300

    update files

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { MessagesModule } from './messages/messages.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+await bootstrap();
